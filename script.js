6ba4141403629fca712ac68fdb9e61a038c7d77e
Małgorzata Nowak <goskanowak87@gmail.com>
2018-12-17 13:33:50 +0100

    Add subpage gallery and library LightBox

--- script.js
+++ script.js
@@ -19,9 +19,40 @@ $(document).ready(function () {
   $('.offer__treatments').on('click', show);
 
   function burger() {
-    console.log('click')
     $('.menu__not-burger').toggleClass('menu__not-burger--active');
   }
-  $('.menu__burger').on('click', burger)
+  $('.menu__burger').on('click', burger);
 
-})
+
+
+
+  var endTime = new Date('2018-12-31 23:59:59').getTime();
+  var spanDays = document.querySelector('.promo__days');
+  var spanHours = document.querySelector('.promo__hours');
+  var spanMins = document.querySelector('.promo__mins');
+  var spanSec = document.querySelector('.promo__sec');
+
+  setInterval(() => {
+      var nowTime = new Date().getTime();
+      var days = Math.floor((endTime / (1000 * 60 * 60 * 24) - (nowTime / (1000 * 60 * 60 * 24))));
+
+      days = days < 10 ? `0${days}` : days;
+
+      var hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
+
+      hours = hours < 10 ? `0${hours}` : hours;
+
+      var mins = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+
+      mins = mins < 10 ? `0${mins}` : mins;
+
+      var secs = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+      secs = secs < 10 ? `0${secs}` : secs;
+
+      spanDays.textContent = days;
+      spanHours.textContent = hours;
+      spanMins.textContent = mins;
+      spanSec.textContent = secs;
+    },
+    1000);
+});
