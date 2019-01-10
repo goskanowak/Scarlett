1d4846e33baa92d0ed293c968b9080916cbd1b06
Małgorzata Nowak <goskanowak87@gmail.com>
2019-01-10 15:35:13 +0100

    Change slider on JS

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 $(document).ready(function () {
 
+  // SLIDER
+  var slideList = [{
+    img: "img/gabinet/DSC_0200.JPG"
+  }, {
+    img: "img/gabinet/DSC_0203.JPG"
+  }, {
+    img: "img/gabinet/DSC_0204.JPG"
+  }, {
+    img: "img/gabinet/DSC_0205.JPG"
+  }, {
+    img: "img/gabinet/DSC_0206.JPG"
+  }, {
+    img: "img/gabinet/DSC_0210.JPG"
+  }, {
+    img: "img/gabinet/DSC_0219.JPG"
+  }, {
+    img: "img/gabinet/DSC_0202.JPG"
+  }]
+
+  var time = 5000;
+  var active = 0;
+  var image = document.querySelector('.slider__img')
+
+  function changeSlide() {
+    active++;
+    if (active == slideList.length) {
+      active = 0;
+    }
+    image.src = slideList[active].img;
+    image.classList.add('slider--anim')
+  }
+
+  setInterval(changeSlide, time);
+
+  //Section burger
+  function burger() {
+    $('.menu__not-burger').toggleClass('menu__not-burger--active');
+  }
+  $('.menu__burger').on('click', burger);
+
+  // section offer 
   function show() {
     if ($('.offer__show', this).text() === '+') {
       console.log('+');
@@ -15,24 +56,17 @@ $(document).ready(function () {
       $('.offer__show', this).text('+');
     }
   }
-
   $('.offer__treatments').on('click', show);
 
-  function burger() {
-    $('.menu__not-burger').toggleClass('menu__not-burger--active');
-  }
-  $('.menu__burger').on('click', burger);
-
-
-
-
-  var endTime = new Date('2018-12-31 23:59:59').getTime();
+  //section promo
+  var endTime = new Date('2019-06-30 23:59:59').getTime();
   var spanDays = document.querySelector('.promo__days');
   var spanHours = document.querySelector('.promo__hours');
   var spanMins = document.querySelector('.promo__mins');
   var spanSec = document.querySelector('.promo__sec');
 
-  setInterval(() => {
+  function countTime() {
+    {
       var nowTime = new Date().getTime();
       var days = Math.floor((endTime / (1000 * 60 * 60 * 24) - (nowTime / (1000 * 60 * 60 * 24))));
 
@@ -53,6 +87,7 @@ $(document).ready(function () {
       spanHours.textContent = hours;
       spanMins.textContent = mins;
       spanSec.textContent = secs;
-    },
-    1000);
+    }
+  }
+  setInterval(countTime, 1000);
 });
